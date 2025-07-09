cfc4c92fe46f6fd504c0d96c49980f578adb2d06
erbsenzoshimizu <159942080+erbsenzoshimizu@users.noreply.github.com>
2025-07-09 13:15:35 -0300

    Melhoria para inutilização das NFCs

--- src/main/java/br/com/erbs/integradorhub/principal/Inutilizacao.java
+++ src/main/java/br/com/erbs/integradorhub/principal/Inutilizacao.java
@@ -2,6 +2,8 @@ package br.com.erbs.integradorhub.principal;
 
 import java.io.File;
 import java.io.StringWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import javax.xml.parsers.DocumentBuilder;
@@ -14,24 +16,18 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class Cancelamento {
+public class Inutilizacao {
 
-    public static void gerarXmlCancelamento(String tpAmb, String cnpj, String chaveNFe, String nProt, String justificativa, String arquivoSaida)
+    /*String tpAmb, String cnpj, String chaveNFe, String nProt, String justificativa, String arquivoSaida*/
+    public static void gerarXmlInutilizacao(String tpAmb, String uf, String cnpj, Integer serie, Integer numero, String justificativa, String arquivoSaida)
             throws Exception {
 
-        // Valida chave
-        if (chaveNFe == null || chaveNFe.length() != 44) {
-            throw new IllegalArgumentException("Chave NFC-e deve ter 44 dígitos, foi passada: " + chaveNFe);
-        }
-
-        String cOrgao = extrairCodigoOrgao(chaveNFe);
-        String idEvento = "ID110111" + chaveNFe + "01";
-        if (!idEvento.matches("ID\\d{52}")) {
-            throw new IllegalStateException("IdEvento inválido: " + idEvento);
-        }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
-        String dhEvento = ZonedDateTime.now().format(formatter);
+        String cUf = obterCodigoUF(uf);
+        String ano = LocalDate.now().format(DateTimeFormatter.ofPattern("yy"));
+        String serieFormatada = String.format("%03d", serie);
+        String numeroFormatado = String.format("%09d", numero);
+        
+        String idEvento = "ID" + cUf + ano + cnpj + "65" + serieFormatada + numeroFormatado + numeroFormatado;
 
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         factory.setNamespaceAware(true);
@@ -39,36 +35,28 @@ public class Cancelamento {
         Document doc = builder.newDocument();
 
         // Cria envEvento com namespace NFC-e
-        Element envento = doc.createElementNS("http://www.portalfiscal.inf.br/nfe", "evento");
-        envento.setAttribute("versao", "1.00");
-        doc.appendChild(envento);
+        Element inutNFe = doc.createElementNS("http://www.portalfiscal.inf.br/nfe", "inutNFe");
+        inutNFe.setAttribute("versao", "4.00");
+        doc.appendChild(inutNFe);
 
         // infEvento
-        Element infEvento = doc.createElement("infEvento");
-        infEvento.setAttribute("Id", idEvento);
-        envento.appendChild(infEvento);
+        Element infInut = doc.createElement("infInut");
+        infInut.setAttribute("Id", idEvento);
+        inutNFe.appendChild(infInut);
 
-        addTag(doc, infEvento, "cOrgao", cOrgao);
-        addTag(doc, infEvento, "tpAmb", tpAmb);
-        addTag(doc, infEvento, "CNPJ", cnpj);
-        addTag(doc, infEvento, "chNFe", chaveNFe);
-        addTag(doc, infEvento, "dhEvento", dhEvento);
-        addTag(doc, infEvento, "tpEvento", "110111");
-        addTag(doc, infEvento, "nSeqEvento", "1");
-        addTag(doc, infEvento, "verEvento", "1.00");
-
-        // detEvento
-        Element detEvento = doc.createElement("detEvento");
-        detEvento.setAttribute("versao", "1.00");
-        infEvento.appendChild(detEvento);
-
-        addTag(doc, detEvento, "descEvento", "Cancelamento");
-        addTag(doc, detEvento, "nProt", nProt);
-        addTag(doc, detEvento, "xJust", justificativa);
+        addTag(doc, infInut, "tpAmb", tpAmb);
+        addTag(doc, infInut, "xServ", "INUTILIZAR");
+        addTag(doc, infInut, "cUF", cUf);
+        addTag(doc, infInut, "ano", ano);
+        addTag(doc, infInut, "CNPJ", cnpj);
+        addTag(doc, infInut, "mod", "65");
+        addTag(doc, infInut, "serie", String.valueOf(serie));
+        addTag(doc, infInut, "nNFIni", String.valueOf(numero));
+        addTag(doc, infInut, "nNFFin", String.valueOf(numero));
+        addTag(doc, infInut, "xJust", justificativa);
 
         // Assinatura fake para testes
         /*adicionarSignatureFake(doc, evento, idEvento);*/
-
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
         transformer.setOutputProperty(OutputKeys.INDENT, "yes");
@@ -78,7 +66,10 @@ public class Cancelamento {
         StringWriter writer = new StringWriter();
         transformer.transform(new DOMSource(doc), new StreamResult(writer));
 
-        arquivoSaida += "Can" + chaveNFe + ".xml";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+        String timestamp = LocalDateTime.now().format(formatter);
+        
+        arquivoSaida += "Inu" + cUf + ano + cnpj + "65" + serieFormatada + numeroFormatado + numeroFormatado + "_" + timestamp + ".xml";
         File outFile = new File(arquivoSaida);
         transformer.transform(new DOMSource(doc), new StreamResult(outFile));
     }
@@ -89,8 +80,69 @@ public class Cancelamento {
         parent.appendChild(element);
     }
 
-    private static String extrairCodigoOrgao(String chaveNFe) {
-        return chaveNFe.substring(0, 2);
+    public static String obterCodigoUF(String sigla) {
+        if (sigla == null) {
+            return null;
+        }
+
+        switch (sigla.trim().toUpperCase()) {
+            case "RO":
+                return "11";
+            case "AC":
+                return "12";
+            case "AM":
+                return "13";
+            case "RR":
+                return "14";
+            case "PA":
+                return "15";
+            case "AP":
+                return "16";
+            case "TO":
+                return "17";
+            case "MA":
+                return "21";
+            case "PI":
+                return "22";
+            case "CE":
+                return "23";
+            case "RN":
+                return "24";
+            case "PB":
+                return "25";
+            case "PE":
+                return "26";
+            case "AL":
+                return "27";
+            case "SE":
+                return "28";
+            case "BA":
+                return "29";
+            case "MG":
+                return "31";
+            case "ES":
+                return "32";
+            case "RJ":
+                return "33";
+            case "SP":
+                return "35";
+            case "PR":
+                return "41";
+            case "SC":
+                return "42";
+            case "RS":
+                return "43";
+            case "MS":
+                return "50";
+            case "MT":
+                return "51";
+            case "GO":
+                return "52";
+            case "DF":
+                return "53";
+            default:
+                return null; // UF inválida
+        }
     }
 
     private static void adicionarSignatureFake(Document doc, Element evento, String idEvento) {
@@ -148,12 +200,13 @@ public class Cancelamento {
 
     public static void main(String[] args) {
         try {
-            gerarXmlCancelamento(
-                    "2",
-                    "17645625003552",
-                    "50250717645625003552650010000000011331052258",
-                    "150250000113154",
-                    "Erro de digitação no CPF do consumidor.",
+            gerarXmlInutilizacao(
+                    "2", 
+                    "MS", 
+                    "17645625003552", 
+                    1, 
+                    2, 
+                    "Mensagem Manual - ERRO DE VALORES", 
                     "C:/temp/"
             );
         } catch (Exception e) {
